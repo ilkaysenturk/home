8eb2b6603add0d9ca6e9e2738a0220fdd466308e
ilkaysenturk-home <ilkaysenturk@yahoo.com>
2020-05-13 20:35:45 +0100

    test

---
+++ exercise.py
@@ -0,0 +1,17 @@
+
+array = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+tuple = (1,2,3)
+if "appel" in array:
+    print("yes")
+else:
+    print("no")
+var="1"
+tuple3=array+tuple
+
+print(type(array))
+print(type(var))
+print(tuple3)
+
+print(array.count("melon"))
+
+print(array.len())
---
+++ first.py
@@ -0,0 +1,10 @@
+
+list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+for x in list:
+ if x=="banana":
+  print ("I like " + x)
+ elif x!="banana":
+  print ("I don't like" + x)
+
+print("assesment complete")
