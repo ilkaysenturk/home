36da931d22c7c829b2ee666479873751b46fc1ae
ilkaysenturk-home <ilkaysenturk@yahoo.com>
2020-05-14 20:45:57 +0100

    Exercises

--- exercise.py
+++ exercise.py
@@ -16,3 +16,50 @@ print(array.count("melon"))
 
 print(array.len())
 ####
+
+
+
+
+cars={"brand": "ford",
+      "model": "mustang",
+      "year": "1964"}
+array=[1,2,3,4,5,6]
+array[5]=7
+print(array)
+
+for x in cars:
+    print(cars[x])
+
+for y in array[0:3]:
+    print(y)
+
+for x,y in cars.items():
+    print(x,y)
+
+if "model" in cars:
+    print("mustang is exist in cars")
+
+    print(len(cars))
+cars["color"]="red"
+print(cars)
+
+del cars["model"]
+
+print(cars)
+
+
+new_cars=cars.copy()
+print(new_cars)
+
+
+
+ChildrenList={
+"child1":{"name":"ilkay",
+           "age":"43"},
+"child2":{"name":"ahmet",
+           "age":"40"},
+"child3":{"name":"mehmet",
+           "age":"27"}
+ }
+
+print(ChildrenList)
--- first.py
+++ first.py
@@ -1,10 +1,1 @@
-
-list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-
-for x in list:
- if x=="banana":
-  print ("I like " + x)
- elif x!="banana":
-  print ("I don't like" + x)
-
-print("assesment complete")
+print("module example")
