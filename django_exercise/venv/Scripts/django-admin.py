1e1aa484b8f621383b922d6d076d706fb7f7edfd
ilkaysenturk-home <ilkaysenturk@yahoo.com>
2020-05-18 11:57:54 +0100

    exercise

---
+++ django_exercise/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ilkay_senturk\git\home\django_exercise\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python_general_exercise/first.py
@@ -0,0 +1,5 @@
+def my_function(n):
+ return lambda a: a*n
+
+doubler=my_function(2)
+tripler=my_function(3)
